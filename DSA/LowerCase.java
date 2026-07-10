dcc06b7aefec9e33261894edbeee32682a065027
Nilam-io <nilmpatil6@gmail.com>
2026-07-10 19:59:51 +0530

    Day 4: Character hashing using arrays

--- DSA/LowerCase.java
+++ DSA/LowerCase.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
 public class LowerCase{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       
     String s = "abcdcabd";
-    int n = 5;
+    
    char queries[]= {'a','b','c','d','e'}; /// imp
     int hash[]= new int[26];
     for(int i=0;i<s.length();i++){
