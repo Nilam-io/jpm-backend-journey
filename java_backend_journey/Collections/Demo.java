e29f51732f37bd44c6b809c17649c52414ccbcc0
Nilam-io <nilmpatil6@gmail.com>
2026-07-16 21:45:00 +0530

    Day:8 Collections

---
+++ java_backend_journey/Collections/Demo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Demo {
+    public static void main(String[] args){
+        Collection<Integer> num = new ArrayList();
+        num.add(5);
+        num.add(6);
+        num.add(8);
+        num.add(34);
+        num.add(23);
+        System.out.println(" "+ num);
+    }
+    
+}
