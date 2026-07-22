95bc676a199129a3b54ea9d4e6810e501e2164a3
Nilam-io <nilmpatil6@gmail.com>
2026-07-22 18:17:39 +0530

    Day 13: Learned Maven basics and created Maven projects

---
+++ java_backend_journey/MAVEN/maven-basic/src/main/java/com/nilam/App.java
@@ -0,0 +1,13 @@
+package com.nilam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
