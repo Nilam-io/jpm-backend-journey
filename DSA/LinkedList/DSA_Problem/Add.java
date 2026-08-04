e11d2c46d18d42b54c2999ce29c36d04db4fed65
Nilam-io <nilmpatil6@gmail.com>
2026-08-04 13:15:30 +0530

    Day: 17 Dsa Problem

--- DSA/LinkedList/DSA_Problem/Add.java
+++ DSA/LinkedList/DSA_Problem/Add.java
@@ -1,11 +1,51 @@
-/*Add two numbers of each value from the two lists */
 
-class ListNode{
+class ListNode {
+
     int val;
     ListNode next;
-    ListNode(int val){
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
         this.val = val;
-        this.next = null;
+    }
+}
+
+class Node {
+
+    private ListNode head;
+
+    public void insert(int data) {
+        ListNode newNode = new ListNode(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        ListNode temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    public ListNode getHead() {
+        return head;
+    }
+
+    public void display() {
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
     }
 }
 
@@ -29,39 +69,56 @@ class Solution {
             int digit = sum % 10;
 
             current.next = new ListNode(digit);
-
             current = current.next;
 
-            if (l1 != null)
+            if (l1 != null) {
                 l1 = l1.next;
+            }
 
-            if (l2 != null)
+            if (l2 != null) {
                 l2 = l2.next;
+            }
         }
 
         return dummy.next;
     }
 }
 
-public class Add{
-    public static void main(String args[]){
-        
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
+public class Add {
 
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-        
+    public static void main(String[] args) {
+
+        Node list1 = new Node();
+
+        list1.insert(2);
+        list1.insert(4);
+        list1.insert(3);
+
+        Node list2 = new Node();
+
+        list2.insert(5);
+        list2.insert(6);
+        list2.insert(4);
+
+        System.out.print("List 1 : ");
+        list1.display();
+
+        System.out.print("List 2 : ");
+        list2.display();
 
         Solution solution = new Solution();
-        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        ListNode result = solution.addTwoNumbers(
+                list1.getHead(),
+                list2.getHead());
+
+        System.out.print("Result : ");
 
         while (result != null) {
             System.out.print(result.val + " ");
             result = result.next;
         }
 
+        System.out.println();
     }
 }
