e57554595cbb685a477738b207c28e05cce3dba4
Nilam-io <nilmpatil6@gmail.com>
2026-08-07 20:04:48 +0530

    Use of Prepared Statements

--- java_backend_journey/JDBC/FirstJDBCapp/src/main/java/org/example/LCApp4.java
+++ java_backend_journey/JDBC/FirstJDBCapp/src/main/java/org/example/LCApp4.java
@@ -1,26 +1,47 @@
 package org.example;
 import java.sql.*;
 import java.sql.DriverManager;
+import java.util.Scanner;
 
 public class LCApp4 {
     public static void main(String[] args) {
         JdbcUtil.loadDriver();
         Connection connect = null;
         Statement statement = null;
-        ResultSet rs = null;
+
+        PreparedStatement pstmnt = null;
         try{
             connect = JdbcUtil.getConnect();
-            statement = connect.createStatement();
+//            statement = connect.createStatement();
 
-            String sql =  "SELECT * FROM student WHERE id = 2";
-            rs = statement.executeQuery(sql);
-            while(rs.next()){
-                System.out.println(
-                        rs.getInt(1) + " " +
-                                rs.getString(2) + " " +
-                                rs.getInt(3) + " " +
-                                rs.getString(4));
-            }
+            String sql =  "Insert into student (id,name,age,city) values(?,?,?,?)";
+               pstmnt =   connect.prepareStatement(sql);
+          System.out.println("Enter the Student data:");
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter the id:");
+          int id =sc.nextInt();
+            System.out.println("Enter the name:");
+            String name =sc.next();
+            System.out.println("Enter the age:");
+            int age =sc.nextInt();
+            System.out.println("Enter the city:");
+            String city =sc.next();
+
+            pstmnt.setInt(1,id);
+            pstmnt.setString(2,name);
+            pstmnt.setInt(3,age);
+            pstmnt.setString(4,city);
+
+           int  rs = pstmnt.executeUpdate();
+           if(rs ==0){
+               System.out.println("Operation failed");
+
+           }
+           else{
+
+               System.out.println("Operation Succesfull");
+           }
+
         }
         catch (SQLException e) {
 
@@ -28,7 +49,7 @@ public class LCApp4 {
 
         } finally {
 
-            JdbcUtil.close(rs);
+
             JdbcUtil.close(statement);
             JdbcUtil.close(connect);
         }
