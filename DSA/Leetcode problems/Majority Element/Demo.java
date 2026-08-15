160d30740284bda9dba348c7ff8a80c8368023d7
Nilam-io <nilmpatil6@gmail.com>
2026-08-15 14:11:57 +0530

    Day:29 Majority Element

---
+++ DSA/Leetcode problems/Majority Element/Demo.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+      
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        int frequency = nums.length / 2;
+
+        
+        for (Integer key : map.keySet()) {
+            if (map.get(key) > frequency) {
+                return key;
+            }
+        }
+
+        return -1;
+    }
+}
+
+public class Demo {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 2, 3};
+        int result = solution.majorityElement(nums);
+        System.out.println("Majority Element: " + result);
+    }
+    
+}
