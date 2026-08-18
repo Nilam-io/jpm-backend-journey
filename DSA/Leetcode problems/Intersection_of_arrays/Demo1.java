9755351a33de32df67d2f375f1aa1a57ed52f962
Nilam-io <nilmpatil6@gmail.com>
2026-08-18 23:06:15 +0530

    Day: 32 Intersection of Array I

--- DSA/Leetcode problems/Intersection_of_arrays/Demo1.java
+++ DSA/Leetcode problems/Intersection_of_arrays/Demo1.java
@@ -1,5 +1,40 @@
-package DSA.Leetcode problems.Intersection_of_arrays;
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+       
+        
+        for(int n : nums1){
+            set1.add(n);
+        }
+        for(int n:nums2){
+            if(set1.contains(n)){
+                set2.add(n);
+            }
+
+        }
+         int result[] = new int[set2.size()];
+         int index = 0;
+         for(int n : set2){
+            result[index] = n;
+            index++;
+         }
+         return result;
+    
+    }
+}
 
 public class Demo1 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int nums1[] = {4,9,5};
+        int nums2[] = {9,4,9,8,4};
+        int result[] = s.intersection(nums1, nums2);
+        for(int n: result){
+            System.out.print(n + " ");
+        }
+    }
     
 }
