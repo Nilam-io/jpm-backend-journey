774066fa62ea7b0a6ba6886ede7e3956f0f819e7
Nilam-io <nilmpatil6@gmail.com>
2026-08-24 23:45:29 +0530

    Buy and sell stocks II

---
+++ DSA/Leetcode problems/Buy or Sell Stocks/Demo2.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+           
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+
+            int profit = prices[i] - minPrice;
+
+            
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int maxProfit = solution.maxProfit(prices);
+        System.out.println("Maximum Profit: " + maxProfit);
+    }
+    
+}
